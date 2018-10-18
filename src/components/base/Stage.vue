fb46fb92aa785c32f61039ee4217b892e5c7f2a0
Zero <zerosyn@gmail.com>
2018-10-18 22:49:02 +0800

    add pen & select tool for stage

--- src/components/base/Stage.vue
+++ src/components/base/Stage.vue
@@ -45,6 +45,8 @@
       @keyup.slash="$modal.show('stage-shortkey')"
       @keyup.backquote="zoom = 1; pan = {x: 0, y: 0};"
       @keyup.space="activateTool('Pan')"
+      @keyup.a="activateTool('Pen')"
+      @keyup.s="activateTool('Select')"
       @keyup.b="activateTool('DrawBG')"
       @keyup.f="activateTool('DrawFG')"
       @keydown.equal="brushSize += 1"
@@ -117,7 +119,9 @@ export default {
     shortkeys: [
       ['/', 'Show this window', true],
       ['`', 'Reset zoom and pan', true],
-      ['Space', 'Pan'],
+      ['Space', 'Pan', true],
+      ['A', 'Foreground Pen'],
+      ['S', 'Select'],
       ['B', 'Draw Background'],
       ['F', 'Draw Foreground'],
       ['1~9', 'Brush size', true],
@@ -138,6 +142,8 @@ export default {
   cursorClip: undefined,
   clip: undefined,
   currentPath: undefined,
+  draggingHandle: false,
+  selectChanged: false,
   undoPaths: [],
 
   computed: {
@@ -157,7 +163,12 @@ export default {
       }
     },
     toolCursor() {
-      return this.currentTool == 'Pan' ? 'move' : 'none';
+      switch (this.currentTool) {
+        case 'Pan':     return 'move';
+        case 'Pen':     return 'crosshair';
+        case 'Select':  return 'default';
+        default:        return 'none';
+      }
     }
   },
   mounted() {
@@ -166,14 +177,15 @@ export default {
 
     paper.install(this);
     paper.setup(this.$refs.canvas);
-    // paper.settings.handleSize = 0;
-    // paper.settings.hitTolerance = 4;
+    paper.settings.hitTolerance = 2;
     this.reset();
     const _this = this;
 
     this.addTool('Pan', {
       onActivate() {
         _this.resetCursor();
+        _this.unselectAll();
+        _this.closeCurrentPath();
       },
       onMouseDrag(event) {
         const offset = event.downPoint.subtract(event.point);
@@ -181,9 +193,232 @@ export default {
         _this.pan = {x: -newCenter.x, y: -newCenter.y};
       }
     });
+    this.addTool('Pen', {
+      onActivate() {
+        _this.resetCursor();
+        _this.unselectAll();
+        _this.currentPath = undefined;
+      },
+      onMouseDown(event) {
+        if (!_this.currentPath || _this.currentPath.closed) {
+          _this.currentPath = (new _this.Path({
+            strokeCap: 'round',
+            strokeJoin: 'round',
+            strokeColor: 'white',
+            fillColor: 'white',
+            fillRule: 'evenodd',
+          })).addTo(_this.clip);
+          
+          _this.currentPath.add(new _this.Segment(event.point));
+          _this.currentPath.fullySelected = true;
+        }
+        if (_this.currentPath.segments.length > 2 &&
+          _this.currentPath.firstSegment.point.isClose(event.point, paper.settings.hitTolerance))
+        {
+          // 不保存，以支持最后调整控制点
+          _this.closeCurrentPath(false);
+          _this.currentPath.firstSegment.selected = true;
+        }
+      },
+      onMouseMove(event) {
+        if (!_this.currentPath || _this.currentPath.closed) return;
+        _this.currentPath.lastSegment.point = event.point;
+      },
+      onMouseDrag(event) {
+        if (!_this.currentPath) return;
+        const offset = event.point.subtract(event.downPoint);
+        const offsetOp = event.downPoint.subtract(event.point);
+        if (_this.currentPath.closed) {
+          // 闭合点只控制结束半边，初始半边即使一开始没有 drag 也不是 0，很难判断
+          _this.currentPath.firstSegment.handleIn = offsetOp;
+        } else {
+          _this.currentPath.lastSegment.handleOut = offset;
+          _this.currentPath.lastSegment.handleIn = offsetOp;
+        }
+      },
+      onMouseUp(event) {
+        if (!_this.currentPath) return;
+        if (_this.currentPath.closed) {
+          _this.closeCurrentPath();
+        } else {
+          // 只显示浮动点的控制点
+          _this.currentPath.lastSegment.selected = false;
+          _this.currentPath.add(new _this.Segment(event.point));
+          _this.currentPath.lastSegment.selected = true;
+        }
+      },
+      onKeyUp(event) {
+        if (event.key == 'escape') {
+          // 点够多自动闭合、提交
+          if (_this.currentPath.segments.length > 2) {
+            _this.closeCurrentPath();
+          } else {
+            _this.currentPath.remove();
+            _this.currentPath = undefined;
+          }
+        }
+      }
+    });
+    this.addTool('Select', {
+      onActivate() {
+        _this.resetCursor();
+        _this.closeCurrentPath();
+      },
+      onMouseDown(event) {
+        _this.draggingHandle = false;
+        _this.selectChanged = false;
+        const hits = _this.clip.hitTestAll(event.point, {
+          tolerance: paper.settings.hitTolerance,
+          fill: true,
+          segments: true,
+          curves: true,
+          handles: true,
+          match: hit => hit.type != 'pixel',  // 忽略 raster
+        });
+        // 如果 hit 包含于当前选中，则不变（保持多选移动）
+        let selectHit = hits.find(hit => hit.item.selected);
+        if (selectHit) {
+          // 选中内部元素时取消其它曲线的选择，并禁止 MouseUp 时循环选择重叠区域
+          if (selectHit.type != 'fill') {
+            _this.selectChanged = true;
+            if (!event.modifiers.shift) {
+              _this.clip.children.forEach(item => {
+                if (item != selectHit.item) {
+                  item.selected = false;
+                }
+              });
+            }
+          }
+        } else {
+          _this.selectChanged = true;
+          // 选中第一个未选中的
+          if (hits.length > 0 && !hits[0].item.selected) {
+            selectHit = hits[0];
+          }
+          if (!event.modifiers.shift) {
+            _this.unselectAll();
+          }
+        }
+        if (selectHit) {
+          selectHit.item.selected = true;
+          if (!selectHit.item.closed) return;  // 非闭合曲线暂时不允许选择内部元素
+          // 选中新的内部元素时取消其它元素的选中状态
+          const unselectSegments = () => {
+            if (!event.modifiers.shift) {
+              selectHit.item.segments.forEach(seg => {
+                seg.selected = false;
+              });
+            }
+          };
+          switch (selectHit.type) {
+            case 'segment':
+              if (!selectHit.segment.selected) {
+                unselectSegments();
+              }
+              selectHit.segment.selected = true;
+              break;
+            case 'curve':
+              if (!selectHit.location.curve.selected) {
+                unselectSegments();
+              }
+              selectHit.location.curve.selected = true;
+              break;
+            case 'handle-in':
+            case 'handle-out':
+              if (!selectHit.segment.selected) {
+                unselectSegments();
+              }
+              selectHit.segment.selected = true;
+              // 另一个控制点取消选中
+              selectHit.segment[selectHit.type == 'handle-in' ? 'handleOut' : 'handleIn'].selected = false;
+              // 必须依靠外部标记，否则单控制点的节点无法区分
+              _this.draggingHandle = true;
+              break;
+            default:
+              unselectSegments();
+          }
+        }
+      },
+      onMouseDrag(event) {
+        const selected = _this.getSelected();
+        if (selected.paths.length == 0) return;
+
+        const delta = event.point.subtract(event.lastPoint);
+        // 只移动控制点
+        if (selected.handles.length == 1 && _this.draggingHandle) {
+          selected.handles[0].set(selected.handles[0].add(delta));
+          return;
+        }
+        // 单曲线选择的情况优先移动点
+        if (selected.paths.length == 1 && selected.segments.length > 0) {
+          selected.segments.forEach(seg => seg.point = seg.point.add(delta));
+          return;
+        }
+        selected.paths.forEach(path => path.translate(delta));
+      },
+      onMouseUp(event) {
+        // 无视已拖动的情况
+        if (!event.point.isClose(event.downPoint, 0)) return;
+        // MouseDown 选中内部且未改变选择的情况循环选择重叠的其它区域
+        if (!_this.selectChanged) {
+          const hits = _this.clip.hitTestAll(event.point, {
+            tolerance: paper.settings.hitTolerance,
+            fill: true,
+            match: hit => hit.type != 'pixel',
+          });
+          if (hits.length <= 1) {
+            return;
+          }
+          const lastSelectIndex = hits.findIndex((hit, index, arr) => {
+            return hit.item.selected && !arr[(index + 1) % arr.length].item.selected
+          });
+          const nextHit = hits[(lastSelectIndex + 1) % hits.length];
+          if (!event.modifiers.shift) {
+            _this.unselectAll();
+          }
+          if (nextHit) {
+            nextHit.item.selected = true;
+          }
+        // 单选结点的情况取消其它结点的选中
+        } else if (!event.modifiers.shift) {
+          const segmentHit = _this.clip.hitTest(event.point, {
+            tolerance: paper.settings.hitTolerance,
+            segments: true,
+            selected: true,
+          });
+          if (segmentHit) {
+            segmentHit.item.segments.forEach(seg => {
+              if (seg != segmentHit.segment) {
+                seg.selected = false;
+              }
+            });
+          }
+        }
+      },
+      onKeyUp(event) {
+        if (event.key == 'delete' || event.key == 'backspace') {
+          const selected = _this.getSelected();
+          if (selected.paths.length == 0) return;
+          // 删除控制点
+          if (selected.handles.length == 1 && _this.draggingHandle) {
+            selected.handles[0]._set(0, 0);
+            return;
+          }
+          // 删除结点
+          if (selected.segments.length > 0) {
+            selected.segments.forEach(seg => seg.remove());
+            return;
+          }
+          // 删除曲线
+          selected.paths.forEach(path => path.remove());
+        }
+      }
+    });
     const drawTool = {
       onActivate() {
         _this.resetCursor();
+        _this.unselectAll();
+        _this.closeCurrentPath();
       },
       onMouseMove(event) {
         _this.cursorClip.position = event.point;
@@ -207,13 +442,15 @@ export default {
           _this.currentPath.closePath();
         }
         _this.currentPath = undefined;
-        _this.save(true);
+        _this.save();
       }
     };
     this.addTool('DrawBG', drawTool);
     this.addTool('DrawFG', drawTool);
     this.activateTool('Pan');
 
+    this.draggingHandle = false;
+    this.selectChanged = false;
     this.undoPaths = [];
   },
   methods: {
@@ -229,6 +466,43 @@ export default {
       const rect = new this.Path.Rectangle(-w, -h, this.size.width, this.size.height);
       this.clip = new this.Group(rect);
       this.clip.clipped = true;  // 所有元素都放在 clip 内才能保证不出界，同时 rasterize 也可以拿到完整的区域
+      // Tool 不支持 DoubleClick，只能放到 clip 上
+      this.clip.onDoubleClick = event => {
+        if (this.currentTool != 'Select') return;
+        // 只有闭合曲线的选中子元素允许双击
+        const hit = this.clip.hitTest(event.point, {
+          tolerance: paper.settings.hitTolerance,
+          segments: true,
+          curves: true,
+          handles: true,
+          selected: true,
+          match: hit => hit.item.closed,
+        });
+        if (!hit) return;
+        switch (hit.type) {
+          // 双击结点切换控制点有空
+          case 'segment':
+            if (hit.segment.hasHandles()) {
+              hit.segment.clearHandles();
+            } else {
+              // 自动根据前后结点生成控制点
+              const p = hit.segment;
+              const vector = p.next.point.subtract(p.previous.point).normalize();
+              hit.segment.handleIn = vector.normalize(-p.point.subtract(p.previous.point).length / 3);
+              hit.segment.handleOut = vector.normalize(p.next.point.subtract(p.point).length / 3);
+            }
+            break;
+          // 双击曲线增加结点
+          case 'curve':
+            hit.location.curve.divideAt(hit.location);
+            break;
+          // 双击控制点删除
+          case 'handle-in':
+          case 'handle-out':
+            hit.segment[hit.type == 'handle-in' ? 'handleIn' : 'handleOut']._set(0, 0);
+            break;
+        }
+      };
       this.resetCursor();
       this.load();
     },
@@ -245,13 +519,14 @@ export default {
     getImg() {
       if (this.clip.children.length > 1) {
         let url;
-        const tempImg = this.clip.rasterize();
+        this.unselectAll();
+        const tempImg = this.clip.rasterize(undefined, false);
         url = tempImg.toDataURL();
         tempImg.remove();
         return url;
       }
     },
-    save(clearUndo = false) {
+    save(clearUndo = true) {
       if (clearUndo) {
         this.undoed = 0;
         this.undoPaths = [];
@@ -267,7 +542,7 @@ export default {
         lastElement.remove();
         this.drew = elements.length - 1;
         this.undoed = 1;
-        this.save();
+        this.save(false);
       }
     },
     redo() {
@@ -275,7 +550,7 @@ export default {
         this.undoPaths.pop().addTo(this.clip);
         this.drew = 1;
         this.undoed = this.undoPaths.length;
-        this.save();
+        this.save(false);
       }
     },
     // 必须是 base64 uri，而不能是跨域 url
@@ -285,8 +560,48 @@ export default {
       if (!emit) return;
       // 莫名其妙这里不能保证立刻获取到 clip 的最终形态，只能强制延后
       setTimeout(() => {
-        this.save(true);
+        this.save();
       }, 0);
+    },
+    closeCurrentPath(alsoSave = true) {
+      // 仅钢笔工具才需要自动闭合（暂且用是否填充来判断）
+      if (!this.currentPath || !this.currentPath.fillColor) return;
+      if (!this.currentPath.closed) {
+        // 闭合时要删除浮动点
+        this.currentPath.lastSegment.remove();
+        this.currentPath.closePath();
+      }
+      if (!alsoSave) return;
+      this.currentPath.selected = false;
+      this.currentPath = undefined;
+      this.drew = 1;
+      this.save();
+    },
+    unselectAll() {
+      this.clip.children.forEach(item => {
+        item.selected = false;
+      });
+    },
+    getSelected() {
+      const selectedPaths = this.clip.getItems({
+        recursive: false,
+        match: item => item.selected,
+      });
+      let selectedSegs = [], selectedHandles = [];
+      if (selectedPaths.length == 1) {
+        selectedSegs = selectedPaths[0].segments.filter(seg => seg.selected);
+        if (selectedSegs.length > 0) {
+          selectedSegs.forEach(seg => {
+            if (seg.handleIn.selected) selectedHandles.push(seg.handleIn);
+            if (seg.handleOut.selected) selectedHandles.push(seg.handleOut);
+          });
+        }
+      }
+      return {
+        paths: selectedPaths,
+        segments: selectedSegs,
+        handles: selectedHandles,
+      };
     },
     addTool(name, methods) {
       const tool = new this.Tool();
@@ -308,7 +623,7 @@ export default {
         pos = this.cursorClip.position;
         this.cursorClip.remove();
       }
-      if (this.currentTool != 'Pan') {
+      if (this.currentTool.startsWith('Draw')) {
         const fg = this.currentTool == 'DrawFG';
         const white = new this.Color(0.9, 0.8);
         const black = new this.Color(0.1, 0.8);
@@ -416,7 +731,8 @@ export default {
       immediate: true,
       handler(val) {
         this.brushSize = clamp(val, 1, this.maxBrush);
-        if (this.currentPath) {
+        // 仅非闭合曲线支持线宽控制
+        if (this.currentPath && !this.currentPath.closed) {
           this.currentPath.strokeWidth = this.brushSize;
         }
         this.resetCursor();
@@ -448,7 +764,7 @@ export default {
 
   button {
     width: 100%;
-    height: 40px;
+    height: 30px;
     margin: 1px 0;
     &:disabled {
       color: #777;
